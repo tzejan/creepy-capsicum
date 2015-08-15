5e822bb693905ae214df938b8a62fdef141b196b
sekheng <150629Z@mymail.nyp.edu.sg>
2015-08-15 19:13:32 +0800

    C++ Assignment 2 Part 5

---
+++ JustForPractice/JustForPractice/JustForPractice.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+using std::string;
+using std::vector;
+
+void BiggestPlot(const unsigned int S, const vector<unsigned short>& prices, size_t& startIndex, unsigned int& count);
+
+void main() {
+        // Part 5
+    int simoleon = 12;
+    const int pricesArray[] = {2,1,3,4,5};
+    vector<unsigned short> prices(pricesArray, pricesArray + sizeof(pricesArray) / sizeof(pricesArray[0]));
+    size_t startIndex = 0;
+    unsigned int count = 0;
+
+    BiggestPlot(simoleon, prices, startIndex, count);
+    cout << "Part 5" << endl;
+    cout << "Land prices = ";
+    for (size_t i = 0; i < prices.size(); ++i)
+    {
+        cout << prices[i] << " ";
+    }
+    cout << endl;
+    cout << simoleon << " simoleons can get you " << count << " plots of land starting from " << startIndex  << endl;
+    cout << "Land bought = ";
+    for (size_t i = startIndex; i < startIndex + count && i < prices.size(); ++i)
+    {
+        cout << prices[i] << " ";
+    }
+    cout << endl << endl;
+}
+
+void BiggestPlot(const unsigned int S, const vector<unsigned short>& prices, size_t& startIndex, unsigned int& count)
+{
+
+    // Refer to Part 5 of Assignment 02
+    startIndex = 0;
+    count = 0;
+}
